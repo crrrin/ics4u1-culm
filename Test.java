ca2ff9bd05f4a0929eea05cc9963f1ec8a51daeb
crrrin <crystalcraft21@gmail.com>
2024-06-07 22:27:51 -0400

    bnlerh

--- Test.java
+++
@@ -1,9 +0,0 @@
-class Test {
-  public static void go() {
-    String s = "";
-    while(s.equals("")) {
-      s = Input.strIn().replaceAll(" ", "");
-    }
-    System.out.println("You entered: " + s);
-  }
-}
